f4ba19b942944140bc0f27139531538a5f75d215
pello198 <kwemoipello@gmail.com>
2025-11-07 11:50:56 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -4,6 +4,15 @@ class RequestLog(models.Model):
     ip_address = models.CharField(max_length=50)
     timestamp = models.DateTimeField(default=timezone.now)
     path = models.CharField(max_length=255)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.country} - {self.city} - {self.path}"
+class RequestLog(models.Model):
+    ip_address = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(default=timezone.now)
+    path = models.CharField(max_length=255)
 
 class BlockedIP(models.Model):
     ip_address = models.CharField(max_length=50, unique=True)
