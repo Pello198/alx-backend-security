56df796816b20a1b2eed83e9166c3c476c5f7b56
pello198 <kwemoipello@gmail.com>
2025-11-07 11:57:44 +0300

    Update settings.py

--- ip_tracking/settings.py
+++ ip_tracking/settings.py
@@ -2,3 +2,11 @@ MIDDLEWARE = [
     ...
     'ip_tracking.middleware.RequestTrackingMiddleware',
 ]
+RATELIMIT_RATES = {
+    'user_or_ip': '10/m',
+    'ip': '5/m',
+}
+INSTALLED_APPS = [
+    ...
+    'ratelimit',
+]
