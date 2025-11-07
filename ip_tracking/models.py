04990b14c9f08addde16d106d3a5055a3649ffb0
pello198 <kwemoipello@gmail.com>
2025-11-07 11:58:50 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.utils import timezone
+class SuspiciousIP(models.Model):
+    ip_address = models.CharField(max_length=50)
+    reason = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.reason}"
 class RequestLog(models.Model):
     ip_address = models.CharField(max_length=50)
     timestamp = models.DateTimeField(default=timezone.now)
