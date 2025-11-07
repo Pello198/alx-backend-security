1894f5445df368de21a9ecc5faf11f447969cf7b
pello198 <kwemoipello@gmail.com>
2025-11-07 11:46:55 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.utils import timezone
+class RequestLog(models.Model):
+    ip_address = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(default=timezone.now)
+    path = models.CharField(max_length=255)
+
+class BlockedIP(models.Model):
+    ip_address = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.ip_address
 
 class RequestLog(models.Model):
     ip_address = models.CharField(max_length=50)
